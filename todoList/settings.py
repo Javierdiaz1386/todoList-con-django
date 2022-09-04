fe77e8fda3c8d51b6b5b271b2134d80dc09e4673
Javierdiaz1386 <javidavi16dd@gmail.com>
2022-09-04 18:30:34 -0500

    Actualizacion 1.0.3

--- todoList/settings.py
+++ todoList/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS =(os.path.join(BASE_DIR, "todoList/static"),)
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'todoList/static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
