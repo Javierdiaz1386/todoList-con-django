b99ff598588430ca3d79d08f065dbc5c0e826faa
Javierdiaz1386 <javidavi16dd@gmail.com>
2022-09-04 21:48:32 -0500

    Creando Un place holder con el plazo

--- todolistApp/models.py
+++ todolistApp/models.py
@@ -4,6 +4,6 @@ class tareasTable(models.Model):
     id = models.AutoField(primary_key=True)
     nombreTarea = models.CharField(max_length=100, verbose_name = 'Nombre de la Tarea')
     fechaIncio = models.DateField(auto_now=True,auto_now_add=False, verbose_name='fechaIncio')
-    plazoParaTerminar = models.DateField(auto_now=False,auto_now_add=False, verbose_name='Plazo Para terminar')
+    plazoParaTerminar = models.DateField(auto_now=False,auto_now_add=False, verbose_name='Plazo Para terminar AAAA-MM-DD')
     def __str__(self):
         return self.nombreTarea
