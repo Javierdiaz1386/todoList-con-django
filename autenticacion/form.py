eea9f61f77f25e39e17d8ee912bce5bd6a5c2956
Javierdiaz1386 <javidavi16dd@gmail.com>
2022-09-07 08:13:59 -0500

    Agregando logins

---
+++ autenticacion/form.py
@@ -0,0 +1,18 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+
+class RegistroForm(UserCreationForm):
+    class Meta:
+        model=User
+        fields=[
+            'first_name',
+            'last_name',
+            'email',
+            'username'
+        ]
+        labels={
+            'first_name': 'Primer Nombre:',
+            'last_name' : 'Segundo Nombre:',
+            'email': 'Correo:',
+            'username': 'Usuario',
+        }
