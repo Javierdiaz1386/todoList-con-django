f9bce8cb84ca7cd20d5c29b9488f403147fee445
Javierdiaz1386 <javidavi16dd@gmail.com>
2022-09-07 09:42:02 -0500

    Filtro de tareas por usuario

--- todolistApp/models.py
+++ todolistApp/models.py
@@ -5,6 +5,6 @@ class tareasTable(models.Model):
     nombreTarea = models.CharField(max_length=40, verbose_name = 'Nombre de la Tarea')
     fechaIncio = models.DateField(auto_now=True,auto_now_add=False, verbose_name='fechaIncio')
     plazoParaTerminar = models.DateField(auto_now=False,auto_now_add=False, verbose_name='Plazo Para terminar AAAA-MM-DD')
-    usuario = models.CharField(max_length=50)
+    usuario = models.CharField(max_length=50, null=True, verbose_name='usuario')
     def __str__(self):
         return self.nombreTarea
